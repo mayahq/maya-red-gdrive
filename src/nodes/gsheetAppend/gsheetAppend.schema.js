09894ef75675c77492906857261f05bf1ec32746
shreyansh073 <shrey.cha@gmail.com>
2022-01-27 14:36:38 +0530

    bug fix

--- src/nodes/gsheetAppend/gsheetAppend.schema.js
+++ src/nodes/gsheetAppend/gsheetAppend.schema.js
@@ -47,6 +47,7 @@ class GsheetAppend extends Node {
         var fetch = require("node-fetch"); // or fetch() is native in browsers
         let len = "https://docs.google.com/spreadsheets/d/".length;
         let spreadsheetId = vals.url.substring(len,vals.url.indexOf('/',len));
+        let  values = [vals.values];
         let fetchConfig = {
             url: `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetId}/values/${encodeURI(vals.range)}:append?insertDataOption=${vals.insertDataOption}&responseDateTimeRenderOption=${vals.responseDateTimeRenderOption}&responseValueRenderOption=${vals.responseValueRenderOption}&valueInputOption=${vals.valueInputOption}`,
             method: "POST",
@@ -57,7 +58,7 @@ class GsheetAppend extends Node {
             body: JSON.stringify({
                     range: vals.range,
                     majorDimension: vals.majorDimension,
-                    values: [vals.values]
+                    values: values
                 })
         }
         try{
